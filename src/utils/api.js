b11553c46f2a08f89c1f1545234298cf544debfb
KaiDev98 <muhammadrifkimms@gmail.com>
2026-06-06 15:58:25 +0700

    menambahkan base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Instance axios dengan base URL & token otomatis
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
 });
 
 // Setiap request otomatis bawa token dari localStorage
